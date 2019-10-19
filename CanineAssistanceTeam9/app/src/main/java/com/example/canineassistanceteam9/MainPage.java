2baefc474aa39ff38b16de6cd5acf487e2d9572d
Chelsea Pierre <cpier114@fiu.edu>
2019-10-19 02:48:51 -0500

    Adding onClick listeners for user options

--- CanineAssistanceTeam9/app/src/main/java/com/example/canineassistanceteam9/MainPage.java
+++ CanineAssistanceTeam9/app/src/main/java/com/example/canineassistanceteam9/MainPage.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,25 +23,20 @@ public class MainPage extends AppCompatActivity implements AdapterView.OnItemCli
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        lv=findViewById(R.id.listv);
-        ArrayAdapter<String> ada=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,days);
-        lv.setAdapter(ada);
-        lv.setOnItemClickListener(this);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Question", Snackbar.LENGTH_LONG)
+                        .setAction("New page", null).show();
             }
         });
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        TextView txt = (TextView) view;
-        Toast.makeText(getApplicationContext(), "u have selected" + txt.getText(), Toast.LENGTH_SHORT).show();
+
+
     }
 
 }
